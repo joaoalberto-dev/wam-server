d50a8ed96fc225b41c52dd7cbb14eb8942cc08a7
João Alberto <hey@joaoalberto.dev>
2025-09-27 01:25:15 -0300

    chore: protecting app with auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,24 @@ import { AppService } from './app.service';
 import { WaitlistsModule } from './waitlists/waitlists.module';
 import { SignupsModule } from './signups/signups.module';
 import { DBService } from './db/db.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WaitlistsModule, SignupsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    WaitlistsModule,
+    SignupsModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, DBService],
+  providers: [
+    AppService,
+    DBService,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+  ],
 })
 export class AppModule {}
